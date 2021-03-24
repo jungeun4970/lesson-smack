83803c9b4a9cd20363e5667bc37e68715bbc327b
JoonChoi <cjg4970@naver.com>
2021-03-24 21:31:35 +0900

    Lesson 3

---
+++ app-smack/app-smack/View/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  app-smack
+//
+//  Created by Choi Joon on 2021/03/24.
+//
+
+import UIKit
+
+//@IBDesignable
+
+class GradientView: UIView {
+
+    @IBInspectable var topColor : UIColor = UIColor.blue{
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottomColor : UIColor = UIColor.green{
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+}
