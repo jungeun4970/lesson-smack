ebacc501f1235168bc521e36fa4d11454dfdca2d
JoonChoi <cjg4970@naver.com>
2021-03-24 22:33:14 +0900

    lesson 4

---
+++ app-smack/app-smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  app-smack
+//
+//  Created by Choi Joon on 2021/03/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ app-smack/app-smack/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  app-smack
+//
+//  Created by Choi Joon on 2021/03/24.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
